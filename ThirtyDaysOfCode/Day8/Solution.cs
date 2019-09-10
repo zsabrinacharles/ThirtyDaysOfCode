5a5c6dc79fffa8cda46867aeea92b583a76eb5e0
Zsabrina C <zsabrina.charles@gmail.com>
2019-09-09 21:32:34 -0400

    Completed Day 8

---
+++ ThirtyDaysOfCode/Day8/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day8
+{
+    class Solution
+    {
+        static Dictionary<string, string> phoneBook = new Dictionary<string,string>();
+        static string SearchPhonebook(string name)
+        {
+            return phoneBook.ContainsKey(name) ? $"{name}={phoneBook[name]}" : "Not found";
+        }
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            int numOfEntries = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < numOfEntries; i++)
+            {
+                var entry = Console.ReadLine().Split(' ');
+                string name = entry[0];
+                string number = entry[1];
+
+                phoneBook.Add(name, number);
+            }
+
+            var searchEntry = Console.ReadLine();
+
+            while(!string.IsNullOrEmpty(searchEntry))
+            {
+                Console.WriteLine(SearchPhonebook(searchEntry));
+
+                searchEntry = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
