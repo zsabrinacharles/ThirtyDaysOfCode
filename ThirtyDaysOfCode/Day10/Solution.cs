6e0eef0f8680b0b31dae3fb284e033bfd104ceef
Zsabrina C <zsabrina.charles@gmail.com>
2019-09-10 21:05:29 -0400

    Completed Day 9 and started Day 10

---
+++ ThirtyDaysOfCode/Day10/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace Day10
+{
+    class Solution
+    {
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
